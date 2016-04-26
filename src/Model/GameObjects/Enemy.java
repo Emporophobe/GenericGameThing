fdb6d7863dd1d43b696308239589de586feef4c8
Michael <emporophobe@gmail.com>
2016-04-25 22:20:03 -0400

    Added max distance enemies will see you from and move towards you

--- src/Model/GameObjects/Enemy.java
+++ src/Model/GameObjects/Enemy.java
@@ -6,23 +6,25 @@ import javafx.scene.paint.Color;
 
 class Enemy extends Living {
 
-    Enemy(){
+    Enemy() {
         super(10, 10, new Point2D(150, 70), 3, 50);
     }
 
     @Override
-    void aiTurn(World w){
+    void aiTurn(World w) {
+        int sightDistance = 100;
+
         Point2D playerPos = w.player.getAABB().getTopLeft();
-        if (position.getX() < playerPos.getX()){
-            setImpulse(new Point2D(1, 0));
-        }
-        else if(position.getX() > playerPos.getX()){
-            setImpulse(new Point2D(-1, 0));
-        }
-        else{
+
+        if (position.distance(playerPos) <= sightDistance) {
+            if (position.getX() < playerPos.getX()) {
+                setImpulse(new Point2D(1, 0));
+            } else if (position.getX() > playerPos.getX()) {
+                setImpulse(new Point2D(-1, 0));
+            }
+        } else {
             setImpulse(new Point2D(0, 0));
         }
-
     }
 
     @Override
